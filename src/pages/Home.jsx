d8c8d60a2a88ec461b3a71a1824e37e4d7eda986
Brian Ochieng <bochieng228@gmail.com>
2025-12-29 12:13:57 +0300

    Styling

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,10 +1,14 @@
 import '../styles/home.css'
-
+import dev from '../assets/image.jpg'
 export default function Home(){
     return(
         <>
         <section class="hero">
             <h1>Festive Season Greetings</h1>
+            <h2>Midusa.Dev</h2>
+            <div className='image'>
+                <img src={dev} alt="developer" className='dev'/>
+            </div>
         </section>
         </>
     )
