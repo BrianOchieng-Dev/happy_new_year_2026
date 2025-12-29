1362a5d6ca1ecf1c5f8568a5837f231c8f54a688
Brian Ochieng <bochieng228@gmail.com>
2025-12-29 11:24:36 +0300

    styling

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,13 +1,11 @@
-
+import '../styles/home.css'
 
 export default function Home(){
     return(
         <>
         <section class="hero">
-
+            <h1>Festive Season Greetings</h1>
         </section>
-        
-        
         </>
     )
 }
