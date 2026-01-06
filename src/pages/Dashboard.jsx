5a316715a84874c4d3e8bcf46d217acbd2969ff3
Brian Ochieng <bochieng228@gmail.com>
2026-01-06 18:13:52 +0300

    dashboard style

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -1,9 +1,14 @@
 
-
+import "../styles/dash.css"
 export default function Dashboard() {
     return(
         <>
         <h1>Send wishes to Midusa</h1>
+        <div className="dash">
+            <form>
+
+            </form>
+        </div>
         </>
     )
 }
